9007df5be55191722fd34c27e9c18971a25e80aa
Shmaiter <shmaither@gmail.com>
2024-02-21 19:46:15 -0600

    (version_02): add exercise 2 of working with beautiful soup

--- version_02/beauty_soup.py
+++ version_02/beauty_soup.py
@@ -1,15 +1,29 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-url = "http://olympus.realpython.org/profiles/dionysus"
-page = urlopen(url)
-html = page.read().decode("utf-8")
-soup = BeautifulSoup(html, "html.parser")
+""" EXERCISE 1 """
+# url = "http://olympus.realpython.org/profiles/dionysus"
+# page = urlopen(url)
+# html = page.read().decode("utf-8")
+# soup = BeautifulSoup(html, "html.parser")
 # print(soup) # the html
 # print(soup.get_text()) # only the html text, without the tags
 
 # img1, img2 = soup.find_all("img") # returns a list with the whole element (tag and content)
-# print(img1.name)
-# print(img1["src"])
+# print(img1.name) # "img"
+# print(img1["src"]) # "/static/dionysus.jpg"
 
-print(soup.tagStack())
+"""EXERCISE 2
+# Write a program that grabs the full HTML from the 
+# page at the URL http://olympus.realpython.org/profiles"""
+
+base_url = "http://olympus.realpython.org"
+html_page = urlopen(base_url + "/profiles")
+html = html_page.read().decode("utf-8")
+soup = BeautifulSoup(html, "html.parser")
+
+links_list = soup.find_all("a")
+
+for link in links_list:
+  link_url = base_url + link["href"]
+  print(link_url)
